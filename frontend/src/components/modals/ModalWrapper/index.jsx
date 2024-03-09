9cbce81e03d049ad8a0d67bb6b5053c4c5d364de
Ankit Kumar <ankitkumardos47@gmail.com>
2024-03-10 00:05:48 +0530

    updated form

--- frontend/src/components/modals/ModalWrapper/index.jsx
+++ frontend/src/components/modals/ModalWrapper/index.jsx
@@ -11,13 +11,19 @@ export default function ModalWrapper({ title, open, setOpen, component }) {
   return (
     <Dialog open={open} onOpenChange={(value) => setOpen(value)}>
       <DialogContent className="max-w-[800px] md:w-[80vw] min-w-[360px] max-h-[80vh]">
-        <DialogHeader className={'h-12'}>
+        <DialogHeader className={"h-20 md:h-16"}>
           <DialogTitle>{title}</DialogTitle>
           <DialogDescription>
-            Book a room for your customer easily. Charges will be calculated on hourly basis even for incomplete hours.
+            <div className="">Book a room for your customer easily.</div>
+            <div className="text-red-500 text-xs">
+              All charges will be calculated on hourly basis even for incomplete
+              hours.
+            </div>
           </DialogDescription>
         </DialogHeader>
-        <div className="max-h-[calc(80vh-6rem)] overflow-y-auto">{component}</div>
+        <div className="max-h-[calc(80vh-10rem)] md:max-h-[calc(80vh-8rem)] overflow-y-auto">
+          {component}
+        </div>
       </DialogContent>
     </Dialog>
   );
